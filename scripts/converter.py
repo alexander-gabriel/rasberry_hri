c2676a2409a241eb3e0276328d9d82997eed12e8
Alexander Gabriel <agabriel@lincoln.ac.uk>
2019-05-28 16:59:04 +0200

    towards joint angles

---
+++ scripts/converter.py
@@ -0,0 +1,78 @@
+
+from numpy import arctan2, abs
+import numpy as np
+
+class Converter:
+
+    def __init__(self):
+        self.angle_joints = {'Right:Elbow-X' : ("RWrist", "RElbow", "RShoulder"),
+                             'Left:Elbow-X' : ("LWrist", "LElbow", "LShoulder"),
+                             'Right:Shoulder-X' : ("Neck", "RShoulder", "RElbow"),
+                             'Left:Shoulder-X' : ("Neck", "LShoulder", "LElbow"),
+                             'Right:Knee-X' : ("RAnkle", "RKnee", "RHip"),
+                             'Left:Knee-X' : ("LAnkle", "LKnee", "LHip"),
+                             'Right:Hip-X' : ("RKnee", "RHip", "LHip"),
+                             'Left:Hip-X' : ("LKnee", "LHip", "RHip"),
+                             'Upper-Spine-X' : ("RHip", "LHip", "Neck"),
+                             'Mid-Spine-X' : ("RHip", "LHip", "Neck"),
+                             'Lower-Spine-X' : ("RHip", "LHip", "Neck"),
+                             'Neck-Z' : ("REar", "LEar", "Neck")}
+
+
+    def create_index_map(self, recognitions):
+        self.index_map = {}
+        self.recognitions = recognitions
+        for index, entry in enumerate(self.recognitions):
+            self.index_map[entry.categorical_distribution.probabilities[0].label] = index
+
+
+    def X(self, label):
+        return self.recognitions[self.index_map[label]].roi.x_offset
+
+
+    def Y(self, label):
+        return self.recognitions[self.index_map[label]].roi.y_offset
+
+
+    def get_angle1(self, joint_name):
+        joints = self.angle_joints[joint_name]
+        v0 = np.array([self.X(joints[0]), self.Y(joints[0])]) - np.array([self.X(joints[1]), self.Y(joints[1])])
+        v1 = np.array([self.X(joints[2]), self.Y(joints[2])]) - np.array([self.X(joints[1]), self.Y(joints[1])])
+
+        angle = np.math.atan2(np.linalg.det([v0,v1]),np.dot(v0,v1))
+        return abs(np.degrees(angle))
+
+
+    def get_position(self, joint_name, model):
+        pass
+
+
+    def get_angle2(self, joint_name):
+        joints = self.angle_joints[joint_name]
+        midX = (self.X(joints[0]) - self.X(joints[1])) / 2.0
+        midY = (self.Y(joints[0]) - self.Y(joints[1])) / 2.0
+        v0 = np.array([self.X(joints[0]), self.Y(joints[0])]) - np.array([midX, midY])
+        v1 = np.array([self.X(joints[2]), self.Y(joints[2])]) - np.array([midX, midY])
+
+        angle = np.math.atan2(np.linalg.det([v0,v1]),np.dot(v0,v1))
+        return abs(np.degrees(angle))
+
+
+    def get_angle3(self, joint_name):
+        joints = self.angle_joints[joint_name]
+        aX = self.X(joints[0]) - self.X(joints[1])
+        bX = self.X(joints[2]) - self.X(joints[1])
+        cX = aX - bX
+        aY = self.Y(joints[0]) - self.Y(joints[1])
+        bY = self.Y(joints[2]) - self.Y(joints[1])
+        cY = aY - bY
+        return abs(arctan2(-cY,cX))
+
+
+    def get_angle4(self, joint_name):
+        joints = self.angle_joints[joint_name]
+        midX = (self.X(joints[0]) - self.X(joints[1]))/ 2.0
+        midY = (self.Y(joints[0]) - self.Y(joints[1]))/2.0
+        cX = self.X(joints[2]) - midX
+        cY = self.Y(joints[2]) - midY
+        return abs(arctan2(-cY,cX))

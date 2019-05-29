831cb3542c2b2f9b0b357b2140f17c99b2be1d24
Alexander Gabriel <agabriel@lincoln.ac.uk>
2019-05-29 16:54:06 +0200

    added forward and backward classes

--- scripts/poses.py
+++ scripts/poses.py
@@ -1,7 +1,7 @@
 
-left_elbow_bent = {"Left:Elbow-X" : 90.0}
-left_elbow_straight = {"Left:Elbow-X" : 180.0}
+forward = {"Left:Shoulder-X": 180.0, "Left:Elbow-X" : 90.0,  "Right:Shoulder-X": 90.0, "Right:Elbow-X" : 180.0 }
+backward = {"Left:Shoulder-X": 90.0, "Left:Elbow-X" : 180.0,  "Right:Shoulder-X": 180.0, "Right:Elbow-X" : 90.0 }
 
 
-pose_list = {"Left Elbow Bent": left_elbow_bent,
-             "Left Elbow Straight": left_elbow_straight}
+pose_list = {"Forward": forward,
+             "Backward": backward}

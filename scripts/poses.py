3de4c834a50caef47c11a5f95cae722329d7d882
Alexander Gabriel <agabriel@lincoln.ac.uk>
2019-05-29 16:37:23 +0200

    typo fix

--- scripts/poses.py
+++ scripts/poses.py
@@ -1,6 +1,6 @@
 
-left_ellbow_bent = {"Left:Elbow-X" : 90.0}
-left_ellbow_straight = {"Left:Elbow-X" : 180.0}
+left_elbow_bent = {"Left:Elbow-X" : 90.0}
+left_elbow_straight = {"Left:Elbow-X" : 180.0}
 
 
 pose_list = {"Left Elbow Bent": left_elbow_bent,
